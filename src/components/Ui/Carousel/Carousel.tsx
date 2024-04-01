f225d1c49bbd0c151de323b159fba30634a97802
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2024-04-01 08:29:58 +0500

    speed banner

--- src/components/Ui/Carousel/Carousel.tsx
+++ src/components/Ui/Carousel/Carousel.tsx
@@ -39,7 +39,7 @@ export default function Carousel() {
           if (mouseOver) return;
           timeout = setTimeout(() => {
             slider.next();
-          }, 20000);
+          }, 3000);
         }
         slider.on('created', () => {
           slider.container.addEventListener('mouseover', () => {
